bd223c3f89815684135a5cb458cf8a36b8ad2aa8
Santos <afonsosantos@deloitte.pt>
2023-11-16 11:27:18 +0000

    pagamento progress

---
+++ programa/AS-pagamento.py
@@ -0,0 +1,8 @@
+class Pagamentos():
+    def __init__(self, valorTotal, metodoPagamento, tarifaReembolsavel, dataPagamento, observacoes, status):
+        self.valorTotal = valorTotal
+        self.metodoPagamento = metodoPagamento
+        self.tarifaReembolsavel = tarifaReembolsavel
+        self.dataPagamentos = dataPagamento
+        self.observacoes = observacoes
+        self.status = status
