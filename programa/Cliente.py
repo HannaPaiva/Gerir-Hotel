a535141ae46d4ec4b78d5763d3f89150d761dfb4
HannaPaiva <hapaiva@deloitte.pt>
2023-11-16 10:34:16 +0000

    arquivos

---
+++ programa/Cliente.py
@@ -0,0 +1,21 @@
+# Crud Clientes
+
+
+class Cliente():
+    
+
+    def Criar_Cliente():
+        pass
+
+    def Ver_Cliente():
+        pass
+
+    def Ver_lista_clientes():
+        pass
+
+    def alterar_Cliente():
+        pass
+
+
+    def apagar_Cliente():
+        pass
