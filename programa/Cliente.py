7c885a6ed6fa40cd2ff7bd24c1a9c6ef04c59c1c
HannaPaiva <hapaiva@deloitte.pt>
2023-11-16 11:02:03 +0000

    alterar coisas

--- programa/Cliente.py
+++ programa/Cliente.py
@@ -1,19 +1,77 @@
 # Crud Clientes
 
 
-class Cliente():
+# class Cliente():
 
-    def criar():
-        pass
+#     def criar():
+#         pass
 
-    def pesquisar():
-        pass
+#     def pesquisar():
+#         pass
 
-    def listar():
-        pass
+#     def listar():
+#         pass
 
-    def alterar():
-        pass
+#     def alterar():
+#         pass
 
-    def apagar():
-        pass
+#     def apagar():
+#         pass
+
+
+
+
+class Cliente:
+    def __init__(self):
+        self.clientes = []
+
+    def criar(self, primeiroNome, nomeDoMeio, ultimoNome, contribuinte, ):
+        novo_cliente = {"nome": nome, "email": email}
+        self.clientes.append(novo_cliente)
+        print("Cliente criado com sucesso!")
+
+    def pesquisar(self, termo):
+        resultados = [cliente for cliente in self.clientes if termo.lower() in cliente['nome'].lower() or termo.lower() in cliente['email'].lower()]
+        return resultados
+
+    def listar(self):
+        if not self.clientes:
+            print("Nenhum cliente cadastrado.")
+        else:
+            for i, cliente in enumerate(self.clientes, 1):
+                print(f"{i}. Nome: {cliente['nome']}, Email: {cliente['email']}")
+
+    def alterar(self, indice, novo_nome, novo_email):
+        if 0 <= indice < len(self.clientes):
+            self.clientes[indice]["nome"] = novo_nome
+            self.clientes[indice]["email"] = novo_email
+            print("Cliente alterado com sucesso!")
+        else:
+            print("Índice inválido.")
+
+    def apagar(self, indice):
+        if 0 <= indice < len(self.clientes):
+            cliente_removido = self.clientes.pop(indice)
+            print(f"Cliente removido: {cliente_removido}")
+        else:
+            print("Índice inválido.")
+
+
+# Exemplo de uso:
+crud_clientes = ClienteCRUD()
+
+crud_clientes.criar("João", "joao@email.com")
+crud_clientes.criar("Maria", "maria@email.com")
+crud_clientes.criar("Carlos", "carlos@email.com")
+
+crud_clientes.listar()
+
+resultado_pesquisa = crud_clientes.pesquisar("jo")
+print("Resultados da pesquisa:")
+print(resultado_pesquisa)
+
+crud_clientes.alterar(1, "João da Silva", "novojoao@email.com")
+crud_clientes.listar()
+
+crud_clientes.apagar(2)
+crud_clientes.listar()
