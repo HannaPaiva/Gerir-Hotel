1ef6e3e7d3677b78724bea332131b262f805a24c
Santos <afonsosantos@deloitte.pt>
2023-11-16 11:58:55 +0000

    pagamentos develop

--- programa/AS_pagamento.py
+++ programa/AS_pagamento.py
@@ -11,4 +11,42 @@ class Pagamentos():
     def valorTotal(self):
         return self.__valorTotal
     
+    @valorTotal.setter
+    def valorTotal(self, valorTotal):
+        if float(valorTotal):
+            self.__valorTotal = valorTotal
+        else:
+            raise
     
+    @property
+    def metodoPagamento(self):
+        return self.__metodoPagamento
+    
+    @metodoPagamento.setter
+    def metodoPagamento(self, metodoPagamento):
+        if isinstance(metodoPagamento , str) and len(metodoPagamento) < 45:
+            self.__metodoPagamento = metodoPagamento
+        else:
+            raise
+    
+    @property
+    def tarifaReembolsavel(self):
+        return self.__tarifaReembolsavel
+    
+    @tarifaReembolsavel.setter
+    def tarifaReembolsavel(self, tarifaReembolsavel):
+        if isinstance(tarifaReembolsavel , int):
+            if tarifaReembolsavel:
+                self.__tarifaReembolsavel = 1
+            else:
+                self.__tarifaReembolsavel = 0
+        else:
+            raise
+        
+    @property
+    def dataPagamento(self):
+        return self.__dataPagamento
+    
+    @dataPagamento.setter
+    def dataPagamento(self, dataPagamento):
+        if 
