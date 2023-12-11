61bc300a0621e710e67f69708107f4b7be611d0c
HannaPaiva <hapaiva@deloitte.pt>
2023-12-11 01:27:34 +0000

    Criação da rota de departamento

--- rotas_funcionarios.py
+++ rotas_funcionarios.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
-from programa.HP_Cliente import *
 from programa.z_database_manager import DatabaseManager
+conn = DatabaseManager(host="127.0.0.1", user="root", password="", database="hotel", port=3306)
 app = Flask(__name__, static_folder='assets', static_url_path='/assets')
 rotas_funcionario = Blueprint("rotas_funcionario", __name__)
-conn= DatabaseManager(host="127.0.0.1", user="root", password="", database="hotel")
+
 
 @rotas_funcionario.route('/funcionarios')
 def listar_funcionarios():
-    action = "pesquisar-funcionario"
     dados = conn.select_data("funcionario")
-    print(dados)
+    action = "pesquisar-funcionario"
     if dados is not None:
         return render_template('funcionarios.html', dados=dados, action = action)
     else:
         return render_template('funcionarios.html')
+    
+
+    
 
 @rotas_funcionario.route('/criar-funcionario', methods=['GET', 'POST'])
 def criar_funcionario():
-    
     dados = {
-        "primeironome": request.form["primeironome"],
-        "nomedomeio": request.form["nomedomeio"],
+        "primeiroNome": request.form["primeiroNome"],
+        "nomeDoMeio": request.form["nomeDoMeio"],
         "ultimoNome": request.form["ultimoNome"],
         "contribuinte": request.form["contribuinte"],
         "CC": request.form["CC"],
@@ -35,8 +36,8 @@ def criar_funcionario():
 
 @rotas_funcionario.route('/editar-funcionario', methods=['GET', 'POST'])
 def editar_funcionario():
+    idfuncionario = {"idfuncionario": request.form["idfuncionario"]}
     dados = {
-        "idfuncionario": request.form["idfuncionario"],
         "primeiroNome": request.form["primeiroNome"],
         "nomeDoMeio": request.form["nomeDoMeio"],
         "ultimoNome": request.form["ultimoNome"],
@@ -48,19 +49,21 @@ def editar_funcionario():
         "ativo": request.form["ativo"],
         "genero": request.form["genero"],
     }
-    atualizar("funcionario", list(dados.keys()), dados, "idfuncionario", dados["idfuncionario"] )
+    conn.update_data("funcionario", dados, idfuncionario)
     return redirect(url_for('rotas_funcionario.listar_funcionarios'))
+
+
 
 @rotas_funcionario.route('/apagar-funcionario', methods=['GET', 'POST'])
 def apagar_funcionario():
-    dados = {
-        "idfuncionario": request.form["idfuncionario"]
-    }
-    apagar("funcionario", "idfuncionario", dados["idfuncionario"])
+    idfuncionario = {"idfuncionario": request.form["idfuncionario"]}
+    conn.delete_data("funcionario", idfuncionario)
     return redirect(url_for('rotas_funcionario.listar_funcionarios'))
 
+
+
 @rotas_funcionario.route('/pesquisar-funcionario', methods=['GET', 'POST'])
-def pesquisar_cliente():
+def pesquisar_funcionario():
     param = request.form["param"]
     # colls = request.form["colls"]
     dados =  conn.select_data(table="funcionario", search= param)
@@ -70,7 +73,9 @@ def pesquisar_cliente():
 
 
 
-app.register_blueprint(rotas_funcionario)
 
+
+
+app.register_blueprint(rotas_funcionario)
 if __name__ == '__main__':
     app.run(debug=True)
